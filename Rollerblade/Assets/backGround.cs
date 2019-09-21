84bf3fcb1683a58512010dd41655edf7f0a12426
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-09-21 19:10:45 +0900

    9/21/~19:10 Mai

---
+++ Rollerblade/Assets/backGround.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+/* 背景画像のリピート(横スクロール） */
+public class BackgroundController : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(-0.1f, 0, 0);
+        if (transform.position.x < -13.8f)
+        {
+            transform.position = new Vector3(13.8f, 0, 0);
+        }
+    }
+}
