a78585c96f3ea428034e522d3dd278109045c8cf
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-22 14:14:58 +0900

    Skillを追加

---
+++ Rollerblade/Assets/User/Masa/Sprites/Skill/InvincibleSkill.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvincibleSkill : Skill
+{
+    [Header("Parameter")]
+    [SerializeField]
+    private PlayerState playerState;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+
+    public override void Activate()
+    {
+        Debug.Log("Skill：無敵");
+        playerState.SetInvincible(3.0f);
+    }
+}
