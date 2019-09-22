8d7d1a2d5d73382884e304a78882864e14420789
MaiKawauchi <s1250110@u-aizu.ac.jp>
2019-09-22 20:15:53 +0900

    GOAL

---
+++ Rollerblade/Assets/Gole.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+
+
+public class Gole : MonoBehaviour
+{
+
+    public GameObject grass;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+    // ぶつかった瞬間に呼び出される
+    void OnTriggerEnter2D(Collider2D c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
--- Rollerblade/Assets/User/Mai/Scripts/Goal.cs
+++ Rollerblade/Assets/User/Mai/Scripts/Goal.cs
@@ -2,17 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gole : MonoBehaviour
+public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject GoalText;
+    public ScrollSystem scrollSystem;
+
+    // Use this for initialization
     void Start()
     {
-        
+
+        GoalText.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+          //  scrollSystem.enabled = !scrollSystem.enabled;
+            GoalText.SetActive(true);
+        }
+    }
+
 }
