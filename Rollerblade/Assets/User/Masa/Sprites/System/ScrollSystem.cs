ac92efcdf7ef71ce9f08735dbf6474d77a4fb119
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-22 19:06:03 +0900

    最適化

---
+++ Rollerblade/Assets/User/Masa/Sprites/System/ScrollSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollSystem : MonoBehaviour
+{
+    [Header("Parameter")]
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private GameObject ScrollObject;
+
+    private void FixedUpdate()
+    {
+        ScrollObject.transform.Translate(-speed * Time.fixedDeltaTime,0,0);
+    }
+}
