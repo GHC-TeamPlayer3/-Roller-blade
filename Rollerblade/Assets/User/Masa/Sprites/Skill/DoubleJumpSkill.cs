d5daa7de824908a123444a39e12b84b7e396a93d
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-24 03:37:35 +0900

    Select画面の実装。Playerの動的生成

---
+++ Rollerblade/Assets/User/Masa/Sprites/Skill/DoubleJumpSkill.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoubleJumpSkill : Skill
+{
+    public override void Activate(PlayerController2D playerController2D)
+    {
+        playerController2D.Jump();
+    }
+}
---
+++ Rollerblade/Assets/User/Masa/Sprites/Skill/SpeedUpSkill.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpSkill : Skill
+{
+    [SerializeField]
+    private float addSpeed = 4.0f;
+
+    public override void Activate(PlayerController2D playerController2D)
+    {
+        playerController2D.scrollSystem.AddSpeed(addSpeed);
+    }
+}
