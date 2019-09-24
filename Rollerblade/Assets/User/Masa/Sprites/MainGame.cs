ad85de7dfc213c647fce9c04d7c07a5b8a0b81ac
Ikeda <MIYAMASA362@yahoo.co.jp>
2019-09-24 13:01:43 +0900

    調整

---
+++ Rollerblade/Assets/User/Masa/Sprites/MainGame.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController2D playerController2D;
+    [SerializeField]
+    private Goal goal;
+    [SerializeField]
+    private GameObject NextLabel;
+
+    private SceneTransition sceneTransition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneTransition = GetComponent<SceneTransition>();
+        NextLabel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerController2D.IsEnd || goal.goalFlag)
+        {
+            NextLabel.SetActive(true);
+            if (Input.GetButtonUp("Fire1"))
+                sceneTransition.SceneChange();
+        }
+    }
+}
